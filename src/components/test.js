17bd55d3c89b2c7d87764041e17fd58a92fce4ac
Furquan712 <smfurquan7.12@gmail.com>
2023-03-06 01:37:46 +0530

    test card updated

--- src/components/test.js
+++ src/components/test.js
@@ -45,19 +45,19 @@ const ThreeCardCarousel = () => {
         </div>
         <div className="flex flex-col md:flex-col p-8  justify-around">
           <Slider {...settings}>
-          <div className="bg-white w-90 rounded-xl shadow-xl m-4 mb-20">
-            <div className="flex flex-row m-4">
-              <Image src={Img}></Image>
-              <h2 className="text-3xl text-center font-bold mb-4  mb-4 ml-6 mt-4">
-                David Anderson
+          <div className="bg-white w-90 rounded-lg shadow-xl m-4 mb-20">
+            <div className="  m-4">
+          
+              <h2 className="text-xl text-[#1E2B5E] text-center font-bold mb-4  mb-4 ml-6 mt-4">
+              A, Lives in Japan
               </h2>
             </div>
 
-            <div className="text-4xl font-bold text-green-500 mb-2 px-16 py-5">
+            <div className="text-4xl font-bold text-green-500 mb-8 px-16 py-5">
               <div className="flex flex-row justify-around">
                 <div className="bg-white rounded-xl shadow-xl   border-4 border-[#1E2B5E] r-0 mr-2">
-                  <h2 className="text-3xl text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
-                    Before
+                  <h2 className="text-base text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
+                    Texbook only
                   </h2>
                   <div className=" mb-2 px-12 py-5">
                     <Image className="w-28" src={Video1}></Image>
@@ -68,8 +68,8 @@ const ThreeCardCarousel = () => {
                   </h2>
                 </div>
                 <div className="bg-white rounded-xl shadow-xl   border-4 border-[#FFC727] ml-2">
-                  <h2 className="text-3xl text-center font-bold text-[#1E2B5E] mb-4 bg-[#ffe680] mb-4">
-                    Before
+                  <h2 className="text-base text-center font-bold text-[#1E2B5E]  bg-[#ffe680] ">
+                  Studrill with textbook
                   </h2>
                   <div className=" mb-2 px-12 py-5">
                     <Image className="w-28" src={Video1}></Image>
@@ -82,26 +82,26 @@ const ThreeCardCarousel = () => {
               </div>
             </div>
             <hr />
-            <div className="flex flex-col m-4 text-center">
-              <p className="text-3xl">I Love Japan</p>
-              <p className="text-3xl">私の名 前は バミカ</p>
-              <p className="text-3xl">Nihon ga suki desu.</p>
+            <div className="flex flex-col mr-10 ml-10 mt-10 mb-20 text-center">
+              <p className="text-3xl">I Love JapanDidn't you say you come from Hawaii?</p>
+              <p className="text-3xl">ハワイ出身って言ってたよね。</p>
+              <p className="text-3xl">Hawai shusshin tte itteta yo ne.</p>
             </div>
           </div>
 
-          <div className="bg-white w-90 rounded-xl shadow-xl m-4 mb-20">
-            <div className="flex flex-row m-4">
-              <Image src={Img}></Image>
-              <h2 className="text-3xl text-center font-bold mb-4  mb-4 ml-6 mt-4">
-                David Anderson
+         <div className="bg-white w-90 rounded-lg shadow-xl m-4 mb-20">
+            <div className="  m-4">
+          
+              <h2 className="text-xl text-[#1E2B5E] text-center font-bold mb-4  mb-4 ml-6 mt-4">
+              A, Lives in Japan
               </h2>
             </div>
 
-            <div className="text-4xl font-bold text-green-500 mb-2 px-16 py-5">
+            <div className="text-4xl font-bold text-green-500 mb-8 px-16 py-5">
               <div className="flex flex-row justify-around">
                 <div className="bg-white rounded-xl shadow-xl   border-4 border-[#1E2B5E] r-0 mr-2">
-                  <h2 className="text-3xl text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
-                    Before
+                  <h2 className="text-base text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
+                    Texbook only
                   </h2>
                   <div className=" mb-2 px-12 py-5">
                     <Image className="w-28" src={Video1}></Image>
@@ -112,8 +112,8 @@ const ThreeCardCarousel = () => {
                   </h2>
                 </div>
                 <div className="bg-white rounded-xl shadow-xl   border-4 border-[#FFC727] ml-2">
-                  <h2 className="text-3xl text-center font-bold text-[#1E2B5E] mb-4 bg-[#ffe680] mb-4">
-                    Before
+                  <h2 className="text-base text-center font-bold text-[#1E2B5E]  bg-[#ffe680] ">
+                  Studrill with textbook
                   </h2>
                   <div className=" mb-2 px-12 py-5">
                     <Image className="w-28" src={Video1}></Image>
@@ -126,26 +126,26 @@ const ThreeCardCarousel = () => {
               </div>
             </div>
             <hr />
-            <div className="flex flex-col m-4 text-center">
-              <p className="text-3xl">I Love Japan</p>
-              <p className="text-3xl">私の名 前は バミカ</p>
-              <p className="text-3xl">Nihon ga suki desu.</p>
+            <div className="flex flex-col mr-10 ml-10 mt-10 mb-20 text-center">
+              <p className="text-3xl">I Love JapanDidn't you say you come from Hawaii?</p>
+              <p className="text-3xl">ハワイ出身って言ってたよね。</p>
+              <p className="text-3xl">Hawai shusshin tte itteta yo ne.</p>
             </div>
           </div>
 
-          <div className="bg-white w-90 rounded-xl shadow-xl m-4 mb-20">
-            <div className="flex flex-row m-4">
-              <Image src={Img}></Image>
-              <h2 className="text-3xl text-center font-bold mb-4  mb-4 ml-6 mt-4">
-                David Anderson
+          <div className="bg-white w-90 rounded-lg shadow-xl m-4 mb-20">
+            <div className="  m-4">
+          
+              <h2 className="text-xl text-[#1E2B5E] text-center font-bold mb-4  mb-4 ml-6 mt-4">
+              A, Lives in Japan
               </h2>
             </div>
 
-            <div className="text-4xl font-bold text-green-500 mb-2 px-16 py-5">
+            <div className="text-4xl font-bold text-green-500 mb-8 px-16 py-5">
               <div className="flex flex-row justify-around">
                 <div className="bg-white rounded-xl shadow-xl   border-4 border-[#1E2B5E] r-0 mr-2">
-                  <h2 className="text-3xl text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
-                    Before
+                  <h2 className="text-base text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
+                    Texbook only
                   </h2>
                   <div className=" mb-2 px-12 py-5">
                     <Image className="w-28" src={Video1}></Image>
@@ -156,8 +156,8 @@ const ThreeCardCarousel = () => {
                   </h2>
                 </div>
                 <div className="bg-white rounded-xl shadow-xl   border-4 border-[#FFC727] ml-2">
-                  <h2 className="text-3xl text-center font-bold text-[#1E2B5E] mb-4 bg-[#ffe680] mb-4">
-                    Before
+                  <h2 className="text-base text-center font-bold text-[#1E2B5E]  bg-[#ffe680] ">
+                  Studrill with textbook
                   </h2>
                   <div className=" mb-2 px-12 py-5">
                     <Image className="w-28" src={Video1}></Image>
@@ -170,10 +170,10 @@ const ThreeCardCarousel = () => {
               </div>
             </div>
             <hr />
-            <div className="flex flex-col m-4 text-center">
-              <p className="text-3xl">I Love Japan</p>
-              <p className="text-3xl">私の名 前は バミカ</p>
-              <p className="text-3xl">Nihon ga suki desu.</p>
+            <div className="flex flex-col mr-10 ml-10 mt-10 mb-20 text-center">
+              <p className="text-3xl">I Love JapanDidn't you say you come from Hawaii?</p>
+              <p className="text-3xl">ハワイ出身って言ってたよね。</p>
+              <p className="text-3xl">Hawai shusshin tte itteta yo ne.</p>
             </div>
           </div>
           </Slider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -37,5 +37,15 @@ module.exports = {
         primary: "#1E2B5E",
       },
     },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '5rem',
+      'full': '9999px',
+      'large': '12px',
+    }
   },
 };
