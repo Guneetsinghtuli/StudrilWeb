55809482dd5cf27815b04731697d26ac02358756
Furquan712 <smfurquan7.12@gmail.com>
2023-03-06 17:06:50 +0530

    icon updated

--- src/components/test.js
+++ src/components/test.js
@@ -82,7 +82,7 @@ const ThreeCardCarousel = () => {
             </div>
           </div>
 
-          <div className="bg-white w-85 rounded-lg mb-20">
+          <div className="bg-white w-85 rounded-lg    mb-20">
             <div className="  m-4">
           
               <h2 className="text-xl text-[#1E2B5E] text-center font-bold mb-4  mb-4 ml-6 mt-4">
@@ -90,27 +90,27 @@ const ThreeCardCarousel = () => {
               </h2>
             </div>
 
-            <div className="text-4xl font-bold text-green-500 mb-8 px-16 py-5">
+            <div className="text-4xl font-bold text-green-500 mb-4 px-16 py-5">
               <div className="flex flex-row justify-around">
-                <div className="bg-white rounded-xl     border-4 border-[#1E2B5E] r-0 mr-2">
-                  <h2 className="text-base text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
-                    Texbook only
+                <div className="bg-white rounded-xl    border-4 border-[#1E2B5E] r-0 mr-2">
+                  <h2 className="text-sm text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
+                    Texbook <br/> only
                   </h2>
-                  <div className=" px-12 py-5">
-                    <Image className="w-28" src={Video1}></Image>
-                  </div>
+                  <a className=" px-10 md:px-8 py-2 w-90">
+                   < BsPlayCircle className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#1E2B5E]"/>
+                  </a>
 
                   <h2 className="text-3xl text-center text-[#1E2B5E] font-bold ">
                     Play
                   </h2>
                 </div>
-                <div className="bg-white rounded-xl     border-4 border-[#FFC727] r-0 mr-2">
-                  <h2 className="text-base text-center font-bold mb-4 text-[#1E2B5E] bg-[#FFC727] mb-4">
-                  Studrill with textbook
+                <div className="bg-white rounded-xl    border-4 border-[#FFC727] r-0 mr-2">
+                  <h2 className="text-sm text-center font-bold mb-4 text-[#1E2B5E] bg-[#FFC727] mb-4">
+                  Studrill with <br/>textbook
                   </h2>
-                  <div className=" px-12 py-5">
-                    <Image className="w-28" src={Video1}></Image>
-                  </div>
+                  <a className=" px-10 md:px-8 py-2 w-90">
+                  < BsPlayCircle className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#EFCB27]"/>
+                  </a>
 
                   <h2 className="text-3xl text-center text-[#1E2B5E] font-bold ">
                     Play
@@ -119,14 +119,14 @@ const ThreeCardCarousel = () => {
               </div>
             </div>
             <hr className="m-2 text-bold"/>
-            <div className="flex flex-col mr-10 ml-10 mt-10 mb-20 text-center">
-              <p className="text-3xl font-medium	text-[#1E2B5E] m-2">I Love JapanDidn't you say you come from Hawaii?</p>
-              <p className="text-3xl font-medium	text-[#1E2B5E] m-2">ハワイ出身って言ってたよね。</p>
-              <p className="text-3xl font-medium	text-[#1E2B5E] m-2">Hawai shusshin tte itteta yo ne.</p>
+            <div className="flex flex-col mr-4 ml-4 mt-2 mb-10 text-center">
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">I Love JapanDidn't you say you come from Hawaii?</p>
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">ハワイ出身って言ってたよね。</p>
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">Hawai shusshin tte itteta yo ne.</p>
             </div>
           </div>
 
-          <div className="bg-white w-85 rounded-lg mb-20">
+           <div className="bg-white w-85 rounded-lg    mb-20">
             <div className="  m-4">
           
               <h2 className="text-xl text-[#1E2B5E] text-center font-bold mb-4  mb-4 ml-6 mt-4">
@@ -134,27 +134,27 @@ const ThreeCardCarousel = () => {
               </h2>
             </div>
 
-            <div className="text-4xl font-bold text-green-500 mb-8 px-16 py-5">
+            <div className="text-4xl font-bold text-green-500 mb-4 px-16 py-5">
               <div className="flex flex-row justify-around">
-                <div className="bg-white rounded-xl     border-4 border-[#1E2B5E] r-0 mr-2">
-                  <h2 className="text-base text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
-                    Texbook only
+                <div className="bg-white rounded-xl    border-4 border-[#1E2B5E] r-0 mr-2">
+                  <h2 className="text-sm text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
+                    Texbook <br/> only
                   </h2>
-                  <div className=" px-12 py-5">
-                    <Image className="w-28" src={Video1}></Image>
-                  </div>
+                  <a className=" px-10 md:px-8 py-2 w-90">
+                   < BsPlayCircle className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#1E2B5E]"/>
+                  </a>
 
                   <h2 className="text-3xl text-center text-[#1E2B5E] font-bold ">
                     Play
                   </h2>
                 </div>
-                <div className="bg-white rounded-xl     border-4 border-[#FFC727] r-0 mr-2">
-                  <h2 className="text-base text-center font-bold mb-4 text-[#1E2B5E] bg-[#FFC727] mb-4">
-                  Studrill with textbook
+                <div className="bg-white rounded-xl    border-4 border-[#FFC727] r-0 mr-2">
+                  <h2 className="text-sm text-center font-bold mb-4 text-[#1E2B5E] bg-[#FFC727] mb-4">
+                  Studrill with <br/>textbook
                   </h2>
-                  <div className=" px-12 py-5">
-                    <Image className="w-28" src={Video1}></Image>
-                  </div>
+                  <a className=" px-10 md:px-8 py-2 w-90">
+                  < BsPlayCircle className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#EFCB27]"/>
+                  </a>
 
                   <h2 className="text-3xl text-center text-[#1E2B5E] font-bold ">
                     Play
@@ -163,10 +163,10 @@ const ThreeCardCarousel = () => {
               </div>
             </div>
             <hr className="m-2 text-bold"/>
-            <div className="flex flex-col mr-10 ml-10 mt-10 mb-20 text-center">
-              <p className="text-3xl font-medium	text-[#1E2B5E] m-2">I Love JapanDidn't you say you come from Hawaii?</p>
-              <p className="text-3xl font-medium	text-[#1E2B5E] m-2">ハワイ出身って言ってたよね。</p>
-              <p className="text-3xl font-medium	text-[#1E2B5E] m-2">Hawai shusshin tte itteta yo ne.</p>
+            <div className="flex flex-col mr-4 ml-4 mt-2 mb-10 text-center">
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">I Love JapanDidn't you say you come from Hawaii?</p>
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">ハワイ出身って言ってたよね。</p>
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">Hawai shusshin tte itteta yo ne.</p>
             </div>
           </div>
           </Slider>
