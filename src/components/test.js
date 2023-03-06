c761a2a5f01b78b380639c69b193c0b9d8d29828
Furquan712 <smfurquan7.12@gmail.com>
2023-03-07 02:43:46 +0530

    audio file

--- src/components/test.js
+++ src/components/test.js
@@ -13,6 +13,12 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BsPlayCircle } from 'react-icons/bs'
+import { useState } from "react";
+import audioFile from ''
+
+// function handleClick(){
+//   setIsPlaying(!isPlaying);
+// }
 
 const ThreeCardCarousel = () => {
   const settings = {
@@ -22,6 +28,8 @@ const ThreeCardCarousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
+  // const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <section className="mt-10 ">
@@ -53,7 +61,12 @@ const ThreeCardCarousel = () => {
                     Texbook <br/> only
                   </h2>
                   <a className=" px-10 md:px-8 py-2 w-90">
-                   < BsPlayCircle className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#1E2B5E]"/>
+                   < BsPlayCircle onClick={handleClick} className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#1E2B5E]"/>
+                    {/* <audio 
+                      src={audioFile}
+                      autoPlay={isPlaying}
+                      onEnded={() => setIsPlaying(false)}
+                    /> */}
                   </a>
 
                   <h2 className="text-3xl text-center text-[#1E2B5E] font-bold ">
@@ -127,6 +140,50 @@ const ThreeCardCarousel = () => {
           </div>
 
            <div className="bg-white w-85 rounded-lg    mb-20">
+            <div className="  m-4">
+          
+              <h2 className="text-xl text-[#1E2B5E] text-center font-bold mb-4  mb-4 ml-6 mt-4">
+              A, Lives in Japan
+              </h2>
+            </div>
+
+            <div className="text-4xl font-bold text-green-500 mb-4 px-16 py-5">
+              <div className="flex flex-row justify-around">
+                <div className="bg-white rounded-xl    border-4 border-[#1E2B5E] r-0 mr-2">
+                  <h2 className="text-sm text-center font-bold mb-4 text-white bg-[#1E2B5E] mb-4">
+                    Texbook <br/> only
+                  </h2>
+                  <a className=" px-10 md:px-8 py-2 w-90">
+                   < BsPlayCircle className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#1E2B5E]"/>
+                  </a>
+
+                  <h2 className="text-3xl text-center text-[#1E2B5E] font-bold ">
+                    Play
+                  </h2>
+                </div>
+                <div className="bg-white rounded-xl    border-4 border-[#FFC727] r-0 mr-2">
+                  <h2 className="text-sm text-center font-bold mb-4 text-[#1E2B5E] bg-[#FFC727] mb-4">
+                  Studrill with <br/>textbook
+                  </h2>
+                  <a className=" px-10 md:px-8 py-2 w-90">
+                  < BsPlayCircle className="w-10 md:w-24   h-auto ml-8 md:ml-12 mr-8 md:mr-12 text-[#EFCB27]"/>
+                  </a>
+
+                  <h2 className="text-3xl text-center text-[#1E2B5E] font-bold ">
+                    Play
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <hr className="m-2 text-bold"/>
+            <div className="flex flex-col mr-4 ml-4 mt-2 mb-10 text-center">
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">I Love JapanDidn't you say you come from Hawaii?</p>
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">ハワイ出身って言ってたよね。</p>
+              <p className="text-4xl font-medium	text-[#1E2B5E] m-2">Hawai shusshin tte itteta yo ne.</p>
+            </div>
+          </div>
+
+          <div className="bg-white w-85 rounded-lg    mb-20">
             <div className="  m-4">
           
               <h2 className="text-xl text-[#1E2B5E] text-center font-bold mb-4  mb-4 ml-6 mt-4">
