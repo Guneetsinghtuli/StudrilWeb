facd9e9eb0e405bd8ada7f0fb5d3a45820420b7b
GUNEET SINGH <guneetsinghtuli@gmail.com>
2023-03-10 20:53:15 +0530

    msg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-
+  console.log("Welcome to Studrill!")
   return (
     <Html lang="en">
       <Head>
