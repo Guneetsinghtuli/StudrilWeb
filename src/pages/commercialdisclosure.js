f24dba0e6bb1450a905c0f8ba111db4019846f3f
GUNEET SINGH <guneetsingh@GUNEETs-MacBook-Air-2.local>
2023-07-14 12:10:21 +0530

    disclosure change

--- src/pages/commercialdisclosure.js
+++ src/pages/commercialdisclosure.js
@@ -9,12 +9,12 @@ const commercialdisclosure = () => {
             <div className="container mx-auto px-8">
 
                 <div className="my-6">
-                    <h1 className="md:text-[28px] text-[24px] font-poppins font-bold text-[#1E2B5E] text-left">Specified Commercial <br /> Transaction Act</h1>
+                    <h1 className="md:text-[28px] text-[24px] font-poppins font-bold text-[#1E2B5E] text-left">Commercial Disclosure <br /> 特定商取引法に基づく表記</h1>
                 </div>
                 <div className="sbka parent">
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Business Name </h2>
+                            <h2>Business name 事業者名</h2>
                         </div>
                         <div className="font-poppins p-3">
                             <h2>Young Serve The World, LLC</h2>
@@ -24,7 +24,7 @@ const commercialdisclosure = () => {
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Service Name </h2>
+                            <h2>Service name サービス名 </h2>
                         </div>
                         <div className="font-poppins p-3">
                             <h2>Studrill</h2>
@@ -34,7 +34,7 @@ const commercialdisclosure = () => {
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Address </h2>
+                            <h2>Address 住所 </h2>
                         </div>
                         <div className="font-poppins p-3">
                             <h2>350-1103 Kawagoe, Saitama, Japan</h2>
@@ -44,7 +44,7 @@ const commercialdisclosure = () => {
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Contact/ Inquiries </h2>
+                            <h2>Contact/ Inquiries 連絡先</h2>
                         </div>
                         <div className="font-poppins p-3">
                             <h2>Email address: contact@studrill.com</h2>
@@ -66,30 +66,40 @@ const commercialdisclosure = () => {
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Representative </h2>
+                            <h2>Representative 代表者 </h2>
                         </div>
                         <div className="font-poppins p-3">
-                            <h2>Studrill LLC</h2>
+                            <h2>アンジェラ・ウィボウォ</h2>
                         </div>
                     </div>
 
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Selling Price </h2>
+                            <h2>Selling price 販売価格 </h2>
                         </div>
                         <div className="font-poppins p-3">
-                            <h2>Depends on the subscribtion it can range from ¥6000 to ¥8000</h2>
+                            <h2>Depending on the subscribtion it can range from ¥6000 to ¥8000.</h2>
                         </div>
                     </div>
 
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Payment Period </h2>
+                            <h2>Payment method 支払方法</h2>
                         </div>
                         <div className="font-poppins p-3">
-                            <h2>Based on the Customers contract with the credit card companies</h2>
+                            <h2>Credit Card</h2>
+                        </div>
+                    </div>
+
+
+                    <div className="singleParent my-4">
+                        <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
+                            <h2>Payment period 支払時期</h2>
+                        </div>
+                        <div className="font-poppins p-3">
+                            <h2>Based on the Customers contract with the credit card companies.</h2>
                         </div>
                     </div>
 
@@ -97,17 +107,17 @@ const commercialdisclosure = () => {
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>Period of Service Provision </h2>
+                            <h2>Period of service provision サービス提供時期 </h2>
                         </div>
                         <div className="font-poppins p-3">
-                            <h2>After the completion of purchase, the first day of learning will start and will end depending upon subscribtion duration that the customer choose</h2>
+                            <h2>After the completion of purchase, the first day of learning will start and will end depending upon subscribtion duration that the customer choose.</h2>
                         </div>
                     </div>
 
 
                     <div className="singleParent my-4">
                         <div className="font-poppins p-3 py-2  font-bold bg-[#1E2B5E] text-white">
-                            <h2>About Withdrwal and Refund</h2>
+                            <h2>About withdrawal and refund 退会・返金について</h2>
                         </div>
                         <div className="font-poppins p-3">
                             <h2>Except in the case of termination of membership by the company, no refund or withdrawl will be made for any fees already paid regardless of whether or not lessons have been taken. </h2>
